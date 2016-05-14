82de7dab4c94c653befbcc21bc0416e1ad289d72
lazenes <lazenes@gmail.com>
2016-05-14 13:53:31 +0300

    Create decoder.php
    How to use / Nasıl Kullanılır?
    
    http://yourdomainname.com/decoder.php?url=BC.VC URL

---
+++ decoder.php
@@ -0,0 +1,18 @@
+<?php 
+ //Single Page PHP Function
+// Bc.Vc Decoder  v0.1 
+// Autor By LaZEnEs
+
+  function deCode($url) {
+        $ch = curl_init();  
+        curl_setopt($ch, CURLOPT_URL, $url);  
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE); 
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);  
+		curl_exec($ch); 
+		$data= curl_getinfo($ch);
+        curl_close($ch); 
+        return $data["redirect_url"];
+    }	
+echo deCode($_GET["url"]);
+
+?>
